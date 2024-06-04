e69c5ce4b08020f765c22b51e9663fbc785ad029
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2024-06-04 10:46:17 +0200

    feat: updated to generic payload format

--- generateDpp.py
+++ generateDpp.py
@@ -3,6 +3,7 @@ import os
 import datetime
 from operators.op import op
 import re
+import copy
 
 DATATYPES = {
     str: "string",
@@ -15,7 +16,7 @@ DATATYPES = {
 }
 
 
-def generateNode(attribute, node, value, label="", description=""):
+def generateNode(attribute, node, value, label="", description="", unit=None):
     tmpLabel = label
     titleLabel = label
     descriptionLabel = description
@@ -24,39 +25,80 @@ def generateNode(attribute, node, value, label="", description=""):
         titleLabel = tmpLabel.title()
         descriptionLabel = tmpLabel.capitalize()
     return createNodes(parent=node, data=value, attr=attribute,
-                        label=titleLabel, description=descriptionLabel)
+                        label=titleLabel, description=descriptionLabel, unit=unit)
     
 
-def createNodes(parent, attr, data, label, description, ref=None, tmpType=None):
-
+def createNodes(parent, attr, data, label, description, ref=None, tmpType=None, unit=None):
     node = createNode(parent=parent, attr=attr, data=data,
-                      label=label, ref=ref, description=label)
+                      label=label, ref=ref, description=label, unit=unit)
 
-    if (node["type"]["datatype"] == "object"):
-        tmpData = {}
-        for attribute in data:
-            value = data[attribute]
-            tmpData[attribute] = generateNode(attribute, node, value)
+    if (node["type"]["dataType"] == "object"):
+        tmpData = list()
+        if("unit" in data and "value" in data):
+            unitValue = copy.deepcopy(data["unit"])
+            valueUnit = copy.deepcopy(data["value"])
+            del data["unit"]
+            del data["value"]
+            tmpData.append(generateValueNode(parent=parent, unit=unitValue, value=valueUnit, ref=ref))
+        
+        for attribute, value in data.items():
+            tmpData.append(generateNode(attribute, node, value, unit))
 
-        node["data"] = tmpData
-    elif(node["type"]["datatype"] == "array"):
-        tmpData = {}
+        node["children"] = tmpData
+        
+    elif(node["type"]["dataType"] == "array"):
+        tmpData = list()
         for attribute, value in enumerate(data):
-            stringIndex = str(attribute)
-            tmpData[stringIndex] = generateNode(stringIndex, node, value, stringIndex, stringIndex)
+            tmpData.append(generateNode(attribute, node, value, attribute, attribute, unit))
 
-        node["data"] = tmpData
+        node["children"] = tmpData
 
     return node
 
+def generateValueNode(parent, unit=None, value=None, ref=None, tmpType=None):
+    global BPN
 
-def createNode(parent, attr, data, label, description, ref=None, tmpType=None):
+    attr = "value"
+    if (tmpType == None):
+        tmpType = {"typeUnit": unit, "dataType": "object"}
+        
+    if (value!=None and type(value) not in DATATYPES):
+        print(f"Invalid data type! in data [{value}]")
+        return None
+
+    datatype = "undefined"
+    if(value != None):
+        datatype = DATATYPES[type(value)]
+
+    tmpType["dataType"] = datatype
+
+    if ref != None:
+        reference = ref
+    elif parent["ref"] == ".":
+        reference = attr
+    elif isinstance(attr, int):
+        reference =  parent["ref"] + "["+str(attr)+"]"
+    else:
+        reference =  ".".join([parent["ref"], attr])
+
+    return {
+        "id": attr,
+        "label": attr,
+        "description": attr,
+        "type": tmpType,
+        "ref": reference,
+        "children": [],
+        "data": value
+    }
+
+
+def createNode(parent, attr, data, label, description, ref=None, tmpType=None, unit=None):
     global BPN
     if (attr == None):
         return None
 
     if (tmpType == None):
-        tmpType = {"unit": None, "datatype": "object"}
+        tmpType = {"typeUnit": unit, "dataType": "object"}
         
     if (data!=None and type(data) not in DATATYPES):
         print(f"Invalid data type! in data [{data}]")
@@ -66,30 +108,36 @@ def createNode(parent, attr, data, label, description, ref=None, tmpType=None):
     if(data != None):
         datatype = DATATYPES[type(data)]
 
-    tmpType["datatype"] = datatype
+    tmpType["dataType"] = datatype
 
     if ref != None:
         reference = ref
-    elif parent["ref"] == "/":
-        reference = "/" + attr
+    elif parent["ref"] == ".":
+        reference = attr
+    elif isinstance(attr, int):
+        reference =  parent["ref"] + "["+str(attr)+"]"
     else:
-        reference =  "/".join([parent["ref"], attr])
+        reference =  ".".join([parent["ref"], attr])
 
+    if(not tmpType["dataType"] in ["array", "object"]):
+        return {
+            "id": attr,
+            "label": label,
+            "description": description,
+            "type": tmpType,
+            "ref": reference,
+            "children": [],
+            "data": data
+        }
     return {
         "id": attr,
         "label": label,
         "description": description,
         "type": tmpType,
         "ref": reference,
-        "audit": {
-            "created": op.timestamp(),
-            "createdBy": BPN,
-            "updated": op.timestamp(),
-            "updatedBy": BPN,
-        },
-        "data": data
+        "children": [],
+        "data": None
     }
-
 
 def generateDataModel(filepath):
     global BPN
@@ -97,7 +145,7 @@ def generateDataModel(filepath):
     initialData = op.readJsonFile(filepath)
 
     dataModel = createNodes(parent={}, attr="passport", data=initialData, label="Digital Product Passport",
-                            description="Root passport node", ref="/")
+                            description="Root passport node", ref=".")
 
     op.toJsonFile(sourceObject=dataModel,
                   jsonFilePath="output/jsonData-"+str(op.timestamp())+".json")
@@ -108,7 +156,7 @@ def generateLabelFromName(name):
 
 
 if __name__ == '__main__':
-    FILE_PATH = "../dataModel.json"
+    FILE_PATH = "./dataModel.json"
     BPN = "user1"
     paramsLen = len(sys.argv)
     if paramsLen > 1:
